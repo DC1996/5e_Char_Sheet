51f67940b5f8e22486dc5fb224ea68f25ff8ce72
dcmayhem <d.czirok99@gmail.com>
2018-07-23 21:13:08 +0200

    Revert "Revert "Ver_0.8.1""
    This reverts commit eb545f8d71542e1bd6afa38e6c016c8e9d1fcf36.

--- lib/DrawerRow.dart
+++ lib/DrawerRow.dart
@@ -6,54 +6,18 @@ class DrawerRow extends FlatButton {
   //definuje a nastaví hodnotu v constructor-e
   final String value; //názov
   final String pageName; //pageName sú definovane main.dart -> routes
-  final String imagePath;
-  DrawerRow(this.value, this.pageName, this.imagePath);
+  DrawerRow(this.value, this.pageName);
 
   @override
   Widget build(BuildContext context) {
-    return new GestureDetector(
-      onTap: () {
-        Navigator.of(context).pushNamed(pageName);
-      },
-      child: new Container(
-        margin: new EdgeInsets.fromLTRB(2.5, 3.0, 2.5, 3.0),
-        padding: new EdgeInsets.all(5.0),
-        decoration: new BoxDecoration(
-          boxShadow: [new BoxShadow(
-            color: Colors.black,
-            blurRadius: 2.0,
-          ),],
-          border: new Border.all(
-            color: Colors.black,
-            width: 4.0,
-          ),
-          color: Color(0xFFececec),
-        ),
-        child: new Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            new Container(
-              child: new Image.asset(imagePath,
-                width: MediaQuery.of(context).size.width * 0.1,
-                height: MediaQuery.of(context).size.width * 0.1,
-              ),
-              width: MediaQuery.of(context).size.width * 0.32,
-            ),
-            new Container(
-                child: new Center(
-                    child: new Text(value,
-                      style: new TextStyle(
-                        color: Colors.black,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    )
-                )
-            ),
-          ],
-        ),
-      ),
+    return new FlatButton(
+        padding: new EdgeInsets.all(2.0),
+        onPressed: (){
+          Navigator.of(context).pushNamed(pageName);
+        },
+        child: new Center(
+          child: new Text(value), //viditelný názov
+        )
     );
   }
 
